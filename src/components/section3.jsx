167660ad975385f18c619dcf63d66184a44a4f75
Harshit Shukla <harshitcr7777@gmail.com>
2024-06-16 21:44:07 +0530

    commiting final changes

--- src/components/section3.jsx
+++ src/components/section3.jsx
@@ -4,7 +4,7 @@ const section3 = () => {
   return (
     <div className="px-4 md:px-6 lg:px-20 py-24">
       <div className="flex font-utendo justify-between items-center gap-6 lg:pb-12 lg:flex-row flex-col">
-        <p className="text-[#0C3506] font-[600] lg:text-[42px] leading-[1] w-full lg:w-2/3 text-center text-[20px] md:text-[32px]">
+        <p className="text-[#0C3506] font-[600] lg:text-[42px] leading-[1] w-full lg:w-2/3 text-center lg:text-start text-[20px] md:text-[32px]">
           Instantly pay merchants by scanning QR codes, making transactions
           faster than ever.
         </p>
